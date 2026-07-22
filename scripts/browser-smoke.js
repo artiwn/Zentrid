d0e1753c5ec252bf791917a5d86d5b469084255b
artiwn <artakiwnl@gmail.com>
2026-07-22 17:03:32 +0400

    Delete scripts directory

--- scripts/browser-smoke.js
+++
@@ -1,1 +0,0 @@
-void require('./browser-e2e').run();
--- scripts/clean-dist.js
+++
@@ -1,5 +0,0 @@
-const { rmSync } = require('fs');
-const { join } = require('path');
-
-rmSync(join(process.cwd(), 'dist'), { recursive: true, force: true });
-console.log('Cleaned dist/.');
